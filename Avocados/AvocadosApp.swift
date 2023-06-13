29dec527cedfec9e35c4ad92d3eb039e4214c4ac
JimSweeneyJr <jimsweeney566@gmail.com>
2023-06-13 19:39:06 -0400

    first commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by James Sweeney on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
